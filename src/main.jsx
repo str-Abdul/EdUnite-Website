a94b660827482785336d0e45b8f0e3204b5e0da0
Abdul Ahad <ahad22671@gmail.com>
2025-09-11 19:57:47 +0500

    Fix routing for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/EdUnite-Website/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
